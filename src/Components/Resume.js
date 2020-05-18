f2cd044a5127ca84d71bd6ec286992c3fb2ec006
duckmoll <ljlin.951010@gmail.com>
2020-05-18 18:57:06 -0400

    init

--- src/Components/Resume.js
+++ src/Components/Resume.js
@@ -1,4 +1,5 @@
 import React, { Component } from 'react';
+import { Progress } from 'semantic-ui-react'
 
 class Resume extends Component {
   render() {
@@ -14,63 +15,59 @@ class Resume extends Component {
         return <div key={work.company}><h3>{work.company}</h3>
             <p className="info">{work.title}<span>&bull;</span> <em className="date">{work.years}</em></p>
             <p>{work.description}</p>
+            <br/>
         </div>
       })
       var skills = this.props.data.skills.map(function(skills){
+        console.log(Number(skills.level.substring(0,2)))
         var className = 'bar-expand '+skills.name.toLowerCase();
-        return <li key={skills.name}><span style={{width:skills.level}}className={className}></span><em>{skills.name}</em></li>
+        return (
+        <li key={skills.name}>
+          <em>{skills.name}</em>
+          <div>
+            <Progress percent={Number(skills.level.substring(0,2))} indicating />
+          </div>
+        </li>)
       })
     }
 
     return (
-      <section id="resume">
-
-      <div className="row education">
-         <div className="three columns header-col">
-            <h1><span>Education</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-            <div className="row item">
-               <div className="twelve columns">
-                 {education}
-               </div>
-            </div>
-         </div>
+  <section id="resume">
+    <div className="row education">
+      <div className="three columns header-col">
+        <h1><span>Education</span></h1>
       </div>
-
-
-      <div className="row work">
-
-         <div className="three columns header-col">
-            <h1><span>Work</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-          {work}
+      <div className="nine columns main-col">
+        <div className="row item">
+            <div className="twelve columns">
+              {education}
+            </div>
         </div>
+      </div>
     </div>
 
 
-
-      <div className="row skill">
-
-         <div className="three columns header-col">
-            <h1><span>Skills</span></h1>
-         </div>
-
-         <div className="nine columns main-col">
-
-            <p>{skillmessage}
-            </p>
-
-				<div className="bars">
-				   <ul className="skills">
-					  {skills}
-					</ul>
-				</div>
-			</div>
+    <div className="row work">
+      <div className="three columns header-col">
+        <h1><span>Work</span></h1>
       </div>
+      <div className="nine columns main-col">
+        {work}
+      </div>
+    </div>
+    <div className="row skill">
+      <div className="three columns header-col">
+        <h1><span>Skills</span></h1>
+      </div>
+      <div className="nine columns main-col">
+        <p>{skillmessage}</p>
+        <div>
+          <ul className="skills">
+            {skills}
+          </ul>
+        </div>
+      </div>
+    </div>
    </section>
     );
   }
